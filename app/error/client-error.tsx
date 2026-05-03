ffc4bbe47428a207b3154ca0fdc2dbd2bd374f32
Guilherme da Silva Benevides <git@gui.dev.br>
2026-05-03 20:57:49 -0300

    feat: implement error handling component with suspense for loading state

---
+++ app/error/client-error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+import { use } from "react";
+
+export default function ErrorContent({
+  searchParams,
+}: {
+  searchParams: Promise<{ message?: string }>;
+}) {
+  const params = use(searchParams);
+  const message = params.message || "Ocorreu um erro desconhecido.";
+  return (
+    <div className="flex flex-col flex-1 justify-center items-center bg-zinc-50 dark:bg-black font-sans">
+      <main className="flex flex-col justify-between items-center sm:items-start bg-white dark:bg-black px-16 py-32 w-full max-w-3xl">
+        <h1 className="mb-4 font-bold text-2xl">Ops ocorreu um erro!</h1>
+        <p className="text-lg">{message}</p>
+        <Link href="/" className="mt-8 btn btn-primary">
+          Voltar para Home
+        </Link>
+      </main>
+    </div>
+  );
+}
--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,20 +1,14 @@
-"use client";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import ErrorContent from "./client-error";
 
-export default function ErrorPage() {
-  const searchParams = useSearchParams();
-  const message =
-    searchParams.get("message") || "Ocorreu um erro desconhecido.";
+export default function ErrorPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ message?: string }>;
+}) {
   return (
-    <div className="flex flex-col flex-1 justify-center items-center bg-zinc-50 dark:bg-black font-sans">
-      <main className="flex flex-col justify-between items-center sm:items-start bg-white dark:bg-black px-16 py-32 w-full max-w-3xl">
-        <h1 className="mb-4 font-bold text-2xl">Ops ocorreu um erro!</h1>
-        <p className="text-lg">{message}</p>
-        <Link href="/" className="mt-8 btn btn-primary">
-          Voltar para Home
-        </Link>
-      </main>
-    </div>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <ErrorContent searchParams={searchParams} />
+    </Suspense>
   );
 }
